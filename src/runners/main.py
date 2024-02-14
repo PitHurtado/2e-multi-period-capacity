8d36d4c8e2fed3594f60722e85375b7ff429e212
phurtac <pitehrh@gmail.com>
2024-02-14 11:11:34 -0500

    add: init main

---
+++ src/runners/main.py
@@ -0,0 +1,14 @@
+"""Main module to solve the Branch and Cut algorithm"""
+
+from src.instance.instance_generator import InstanceGenerator
+from src.runners.main_branch_and_cut import Main
+
+if __name__ == "__main__":
+    # (1) Generate instance:
+    instances = InstanceGenerator().get_instances()
+
+    # (2) Solve:
+    run_time = 3600
+    for instance in instances:
+        main = Main(run_time)
+        main.solve(instance)
