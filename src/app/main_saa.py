c2d82d918cc0c3746d9404d7738a863d1560d019
phurtac <pitehrh@gmail.com>
2024-02-16 09:20:03 -0500

    add: main saa

---
+++ src/app/main_saa.py
@@ -0,0 +1,32 @@
+"""Main module for the SAA application."""
+import logging
+from typing import Any, Dict, List
+
+from src.instance.experiment import Experiment
+from src.model.sample_average_approximation import SampleAverageApproximation
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+if __name__ == "__main__":
+    # (1) Generate instance:
+    folder_path = "../results/saa/"
+    logger.info("[MAIN SAA] Generating instances")
+    instances_generated: List[Dict[str, Any]] = Experiment(
+        N_evaluation=2, M=2, folder_path=folder_path
+    ).generate_instances()  # TODO change this
+    logger.info("[MAIN SAA] Instances generated")
+
+    # (2) Solve and save results:
+    run_time = 3600
+    for i, experiment in enumerate(instances_generated):
+        id_experiment = (1 + i) * 100
+        logger.info(f"[MAIN SAA] Experiment {id_experiment} started")
+        solver_saa = SampleAverageApproximation(
+            experiment=experiment,
+            id_experiment=id_experiment,
+        )
+        solver_saa.run()
+        logger.info(f"[MAIN SAA] Experiment {id_experiment} finished")
