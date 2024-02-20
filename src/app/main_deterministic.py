7872443c4815fd178494d981465ceb00fcec6c7c
phurtac <pitehrh@gmail.com>
2024-02-19 21:47:38 -0500

    fix: main run deterministic

--- src/app/main_deterministic.py
+++ src/app/main_deterministic.py
@@ -33,20 +33,6 @@ if __name__ == "__main__":
     model.build()
     model.solve()
 
-    # print("Objective function value: ", model.objective.getValue())
-    # print(
-    #     "cost_allocation_satellites value: ",
-    #     model.cost_allocation_satellites.getValue(),
-    # )
-    # print(
-    #     "cost_operating_satellitesn value: ", model.cost_operating_satellites.getValue()
-    # )
-    # print(
-    #     "cost_served_from_satellite value: ",
-    #     model.cost_served_from_satellite.getValue(),
-    # )
-    # print("cost_served_from_dc value: ", model.cost_served_from_dc.getValue())
-
     # (3) Save results:
     Y_solution = {str(keys): value.X for keys, value in model.model._Y.items()}
     Y_solution["objective"] = model.objective.getValue()
